1b776b092b71d50ff229715c89af9b80de619fdb
charain <charain_li@outlook.com>
2024-09-28 10:25:35 +0800

    update

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My custom Nix packages";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs }:
