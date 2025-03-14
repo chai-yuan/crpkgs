e94d67f8799e03ef903132672dc207a7c20f962a
charain <charain_li@outlook.com>
2025-03-14 14:21:08 +0800

    添加riscv64-glibc-gcc

--- packages/default.nix
+++ packages/default.nix
@@ -4,5 +4,6 @@
   hello = import ./hello { inherit pkgs; };
   neovim = import ./neovim { inherit pkgs; };
   riscv64-elf-gcc = import ./riscv64-elf-gcc { inherit pkgs; };
+  riscv64-glibc-gcc = import ./riscv64-glibc-gcc { inherit pkgs; };
   riscv32-elf-gcc = import ./riscv32-elf-gcc { inherit pkgs; };
 }
---
+++ packages/riscv64-glibc-gcc/default.nix
@@ -0,0 +1,29 @@
+{ pkgs }:
+
+pkgs.stdenv.mkDerivation rec {
+  pname = "riscv64-glibc-gcc";
+
+  src = pkgs.fetchurl {
+    url = "https://toolchains.bootlin.com/downloads/releases/toolchains/riscv64-lp64d/tarballs/riscv64-lp64d--glibc--stable-2024.05-1.tar.xz";
+    sha256 = "0cj000sis87x8hxzj4zq2swrhdf1s857a2gfa937rlwrc98jphqp";  
+  };
+
+  unpackPhase = ''
+    tar xf $src
+  '';
+
+  installPhase = ''
+    mkdir -p $out
+    cp -r riscv64-lp64d--glibc--stable-2024.05-1/* $out/
+  '';
+
+  shellHook = ''
+  '';
+
+  meta = with pkgs.lib; {
+    description = "RISC-V GNU Toolchain (precompiled)";
+    homepage = "https://github.com/riscv-collab/riscv-gnu-toolchain";
+    license = licenses.gpl3;
+    platforms = platforms.linux;
+  };
+}
