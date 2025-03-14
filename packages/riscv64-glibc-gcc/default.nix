54ea2feb8e056223b6633a28ac06eb5628fb340b
charain <charain_li@outlook.com>
2025-03-14 14:30:48 +0800

    fix riscv64-glibc-gcc

--- packages/riscv64-glibc-gcc/default.nix
+++ packages/riscv64-glibc-gcc/default.nix
@@ -2,6 +2,7 @@
 
 pkgs.stdenv.mkDerivation rec {
   pname = "riscv64-glibc-gcc";
+  version = "2024.05-1";
 
   src = pkgs.fetchurl {
     url = "https://toolchains.bootlin.com/downloads/releases/toolchains/riscv64-lp64d/tarballs/riscv64-lp64d--glibc--stable-2024.05-1.tar.xz";
@@ -21,7 +22,7 @@ pkgs.stdenv.mkDerivation rec {
   '';
 
   meta = with pkgs.lib; {
-    description = "RISC-V GNU Toolchain (precompiled)";
+    description = "RISC-V GNU Toolchain (precompiled) with glibc";
     homepage = "https://github.com/riscv-collab/riscv-gnu-toolchain";
     license = licenses.gpl3;
     platforms = platforms.linux;
